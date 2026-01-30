1a49359d4621c0168a03edbc38d794e0c9a2ab5a
Rohit Kumar <kumarrohit23502@gmail.com>
2026-01-30 18:09:24 +0530

    add hostinger creditianls

--- server/utils/mailSender.js
+++ server/utils/mailSender.js
@@ -1,26 +1,26 @@
 const nodemailer = require("nodemailer");
 
 const mailSender = async (email, title, body) => {
-  try {
-    let transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,       // smtp.gmail.com
-  port: 465,                          // SSL port
-  secure: true,                       // true for 465, false for 587
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,      // Gmail App Password
-  },
-});
-
-    let info = await transporter.sendMail({
-      from: "StudyNotion || CodeHelp - by Babbar",
-      to: `${email}`,
-      subject: `${title}`,
-      html: `${body}`,
-    });
-    console.log(info);
-    return info;
-  } catch (error) {
+    try{
+          let transporter=nodemailer.createTransport({
+              host:process.env.MAIL_HOST,
+              port: 587,
+              secure: false, // true for 465, false for other ports
+              auth:{
+                  user:process.env.MAIL_USER,
+                  pass:process.env.MAIL_PASS
+              }
+          });
+  
+          let info=await transporter.sendMail({
+              from: `"Elevate Hub Modern" <${process.env.MAIL_USER}>`,
+              to:`${email}`,
+              subject:`${title}`,
+              html:`${body}`
+          })
+          console.log(info);
+          return info;
+        } catch (error) {
     console.log(error.message);
     throw error;
   }
